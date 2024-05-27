425dcbe0b497b80b830f5587f7ad10e5623a6737
박시현 <sihyun615@naver.com>
2024-05-27 23:59:17 +0900

    댓글 수정

--- src/main/java/com/sparta/schedule/controller/CommentController.java
+++ src/main/java/com/sparta/schedule/controller/CommentController.java
@@ -1,12 +1,16 @@
 package com.sparta.schedule.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sparta.schedule.dto.CommentRequestDto;
 import com.sparta.schedule.dto.CommentResponseDto;
+import com.sparta.schedule.dto.ScheduleRequestDto;
 import com.sparta.schedule.service.CommentService;
 
 import lombok.RequiredArgsConstructor;
@@ -23,4 +27,11 @@ public class CommentController {
 	public CommentResponseDto createComment(@RequestBody CommentRequestDto requestDto) {
 		return commentService.createComment(requestDto);
 	}
+
+	// 댓글 수정
+	@PutMapping("/comment/{commentId}")
+	public CommentResponseDto updateComment(@PathVariable Long commentId, @RequestBody CommentRequestDto requestDto) {
+		return commentService.updateComment(commentId, requestDto);
+	}
+
 }
--- src/main/java/com/sparta/schedule/service/CommentService.java
+++ src/main/java/com/sparta/schedule/service/CommentService.java
@@ -4,6 +4,7 @@ import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.sparta.schedule.dto.CommentRequestDto;
 import com.sparta.schedule.dto.CommentResponseDto;
@@ -49,6 +50,55 @@ public class CommentService {
 
 		Comment comment = new Comment(content, userId, schedule);
 		commentRepository.save(comment);
+		return new CommentResponseDto(comment);
+	}
+
+	@Transactional
+	public CommentResponseDto updateComment(Long commentId, CommentRequestDto requestDto) {
+		Long scheduleId = requestDto.getScheduleId();
+		if (scheduleId == null) {
+			throw new IllegalArgumentException("댓글을 작성할 일정의 ID가 입력되지 않았습니다.");
+		}
+
+		if (commentId == null) {
+			throw new IllegalArgumentException("댓글의 ID가 입력되지 않았습니다.");
+		}
+
+		String newContent = requestDto.getContent();
+		if (newContent.isEmpty()) {
+			throw new IllegalArgumentException("댓글의 내용이 비어있습니다.");
+		}
+
+		String userId = requestDto.getUserId();
+		if (userId.isEmpty()) {
+			throw new IllegalArgumentException("작성자 ID가 입력되지 않았습니다.");
+		}
+
+
+		// 선택한 일정이 DB에 저장되어 있는지 확인
+		Optional<Schedule> checkSchedule = scheduleRepository.findById(scheduleId);
+		if (checkSchedule.isEmpty()) {
+			throw new NoSuchElementException("선택한 일정을 찾을 수 없습니다.");
+		}
+
+		// 선택한 댓글이 DB에 저장되어 있는지 확인
+		Optional<Comment> checkComment = commentRepository.findById(commentId);
+		if (checkComment.isEmpty()) {
+			throw new NoSuchElementException("선택한 댓글을 찾을 수 없습니다.");
+		}
+
+		Comment comment = checkComment.get();
+
+		if (!comment.getUserId().equals(userId)) {
+			throw new IllegalArgumentException("선택한 댓글의 사용자가 현재 사용자와 일치하지 않습니다.");
+		}
+
+		// 댓글 내용이 변경되었는지 확인하고 변경된 경우에만 업데이트
+		if (!newContent.equals(comment.getContent())) {
+			comment.setContent(newContent);
+			commentRepository.save(comment);
+		}
+
 		return new CommentResponseDto(comment);
 	}
 }
